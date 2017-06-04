602c8f2fa0d6a3d7a8f779d4f9699b769535f5c8
liu-xing.1234@163.com <liu-xing.1234@163.com>
2017-06-04 10:19:30 +0800

    first commit

---
+++ myoper/views.py
@@ -0,0 +1,8 @@
+#coding=utf-8
+from django.shortcuts import render,render_to_response
+from django.http import HttpResponse,HttpResponseRedirect
+from django.template import RequestContext
+from models import T_USER
+from django import forms
+from flask.wrappers import Response
+# Create your views here.
